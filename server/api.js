116d03511e1684a3e11faefd62aeedef66d2ef14
Alessio Hu <kirbyalessio@yahoo.it>
2023-09-02 00:49:34 +0200

    No local ocgsets

--- server/api.js
+++ server/api.js
@@ -757,7 +757,7 @@ export default app
 	}
 
 	async function updateWithOCG(allsets, allcards, allcardsToT) {
-		const Local = true
+		const Local = false
 		const ocgsets = Local
 			? JSON.parse(fs.readFileSync("server/data/ocgsets.json").toString())
 			: await new Promise((resolve, reject) => {
